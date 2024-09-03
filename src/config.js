08503530a462e2d45b1f541097fe15cedc8a3649
= <=>
2024-09-03 14:36:02 +0100

    added

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export const allconfig = {
-    env: 'prod',
+    env: 'dev',
     dev_api_url : 'http://localhost:8000',
     dev_site_url: 'http://localhost:3001',
-    prod_api_url : 'https://studentcomplaint-admin.vercel.app',
+    prod_api_url : 'https://studentcomplaint-server.vercel.app',
     prod_site_url: 'http://dashboard-xyz.cryptolits.org',
     prod_landing_url: 'http://cryptolits.com',
 }
